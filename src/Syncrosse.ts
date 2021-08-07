4d45f5777de9dc4f0bf9c81898092f28fc64c120
Loganarmack <loganarmack@gmail.com>
2021-08-06 22:55:13 -0400

    got testing working

--- src/Syncrosse.ts
+++ src/Syncrosse.ts
@@ -3,6 +3,7 @@ import * as Io from 'socket.io';
 
 export class Syncrosse {
   private server: Io.Server;
+  private actions: { [key: string]: (data: any) => void } = {};
 
   constructor(http: Server) {
     (this.server = new Io.Server(http)),
@@ -12,7 +13,11 @@ export class Syncrosse {
   }
 
   public onAction(action: string, callback: (data: any) => void): void {
-    this.server.on(action, callback);
+    const restricted = ['connection', 'disconnect', 'message'];
+    if (restricted.includes(action)) {
+      throw new Error(`${action} is a reserved action`);
+    }
+    this.actions[action] = callback;
   }
 
   public triggerEvent(event: string, data?: any, opts?: { except: UserId[] } | { only: UserId[] }): void {
@@ -31,13 +36,17 @@ export class Syncrosse {
   }
 
   public onJoin(callback: (user: UserId) => void): void {
-    this.server.on('connection', ({ id }) => {
-      callback(id);
+    this.server.on('connection', (socket) => {
+      console.log('Added actions on connection');
+      for (const action in this.actions) {
+        socket.on(action, this.actions[action]);
+      }
+      callback(socket.id);
     });
   }
 
   public onLeave(callback: (user: UserId) => void): void {
-    this.server.on('disconnect', callback);
+    this.onAction('disconnect', callback);
   }
 }
 
--- src/tests/Syncrosse.test.ts
+++ src/tests/Syncrosse.test.ts
@@ -1,10 +1,49 @@
 import { Syncrosse } from '../index';
-import { Server } from 'http';
+import { createServer } from 'http';
+//import { Syncrosse as Client } from '@syncrosse/client';
+const Client = require('socket.io-client');
+import { Server, Socket } from 'socket.io';
 
 describe('Syncrosse', () => {
+  const httpServer = createServer();
+
   it('should not return null', () => {
-    const http = new Server();
-    const client = new Syncrosse(http);
-    expect(client).not.toBeNull();
+    const syncrosse = new Syncrosse(httpServer);
+    expect(syncrosse).not.toBeNull();
+  });
+
+  it('should be able to create actions', () => {
+    const syncrosse = new Syncrosse(httpServer);
+    syncrosse.onAction('ping', () => {});
+
+    //@ts-ignore
+    expect(syncrosse.actions.ping).not.toBeNull();
+  });
+
+  it('should trigger events when called', async () => {
+    const syncrosse = new Syncrosse(httpServer);
+    const spy = jest.fn();
+
+    let clientSocket: any;
+
+    await new Promise((res) => {
+      syncrosse.onAction('ping', () => {
+        console.log('pong');
+        spy();
+        res(null);
+      });
+      syncrosse.onJoin(() => {});
+      httpServer.listen(() => {
+        //@ts-ignore
+        const port = httpServer.address()!.port;
+        clientSocket = new Client(`http://localhost:${port}`);
+        clientSocket.on('connect', () => {});
+        clientSocket.emit('ping');
+      });
+    });
+    expect(spy).toHaveBeenCalled();
+    //@ts-ignore
+    syncrosse.server.close();
+    clientSocket.close();
   });
 });
